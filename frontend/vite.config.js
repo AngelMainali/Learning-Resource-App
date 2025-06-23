cf6be3eb519d73ffe7e235a285c11e66a0be00dc
AngelMainali <anjelmainali11@gmail.com>
2025-06-23 15:59:00 +0545

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,15 +9,6 @@ export default defineConfig({
     include: /src\/.*\.[jt]sx?$/,
     exclude: [],
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   // Ensure public assets are served correctly
   publicDir: "public",
 })
